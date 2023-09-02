9c2623fd9a44aac36464636a9bc88258780cdad7
Ansderly Rameau <rameauhansderly@gmail.com>
2023-09-01 23:20:54 -0400

    Move getUserFunc function to func\user.ts

--- src/func/auth.ts
+++ src/func/auth.ts
@@ -1,11 +1,1 @@
 import { prisma } from '../db';
-
-const getUserFunc = (phone: string) => {
-  const user = prisma.user.findUnique({ where: { phone } });
-
-  if (!user) return null;
-
-  return user;
-};
-
-export { getUserFunc };
