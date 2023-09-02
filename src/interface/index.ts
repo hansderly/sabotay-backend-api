0b9a9a0e4ba7021cbdb6767b01dbc6b3436f296c
Ansderly Rameau <rameauhansderly@gmail.com>
2023-09-02 13:55:16 -0400

    Add User and organizer interface

---
+++ src/interface/index.ts
@@ -0,0 +1,14 @@
+// USER INTERFACE
+interface User {
+  first_name: string;
+  last_name: string;
+  phone: string;
+}
+
+interface Organizer {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export { User, Organizer };
