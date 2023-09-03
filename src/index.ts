49a545e0880eae91853ed6d94f6ce21a4d5b1050
Ansderly Rameau <rameauhansderly@gmail.com>
2023-09-02 20:37:45 -0400

    Add group routes and import groupRoutes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { authRoutes } from './routes';
+import { authRoutes, groupRoutes } from './routes';
 
 const app = express();
 app.use(express.json());
 
 app.use(authRoutes);
+app.use(groupRoutes);
 
 const PORT = 2023;
 app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
--- src/routes/group.ts
+++ src/routes/group.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import {
+  allGroup,
+  createGroup,
+  deleteGroup,
+  groupDetail,
+} from '../controllers';
 
 const router = express.Router();
 
-router.post('/api/groups');
-router.get('/api/groups');
-router.get('/api/groups/:group_id');
+router.post('/api/groups', createGroup);
+router.get('/api/groups', allGroup);
+router.get('/api/groups/:group_id', groupDetail);
 router.put('/api/groups/:group_id');
-router.delete('/api/groups/:group_id');
+router.delete('/api/groups/:group_id', deleteGroup);
 
 export default router;
