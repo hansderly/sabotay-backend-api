dc2c0f0426b448e1eb15164e8980d541b697a6e5
Ansderly Rameau <rameauhansderly@gmail.com>
2023-09-03 01:36:52 -0400

    Add function for user controller such as getUser, getUserById, createuser, deleteUser and getAllUser

--- src/func/user.ts
+++ src/func/user.ts
@@ -1,16 +1,27 @@
 import { User } from '../interface';
 import { prisma } from '../db';
 
-const getUserFunc = (phone: string) => {
-  const user = prisma.user.findUnique({ where: { phone } });
+const getUser = async (phone: string) => {
+  const user = await prisma.user.findUnique({ where: { phone } });
 
   if (!user) return null;
 
   return user;
 };
 
-const createUserFunc = (user: User) => {
-  prisma.user.create({ data: user });
+const getUserById = async (id: string) => {
+  const user = await prisma.user.findUnique({ where: { id } });
+
+  if (!user) return null;
+
+  return user;
 };
 
-export { getUserFunc, createUserFunc };
+const createUser = async (user: User) => prisma.user.create({ data: user });
+
+const deleteUser = async (user_id: string) =>
+  prisma.user.delete({ where: { id: user_id } });
+
+const getAllUser = async () => prisma.user.findMany();
+
+export { getUser, getUserById, createUser, getAllUser, deleteUser };
