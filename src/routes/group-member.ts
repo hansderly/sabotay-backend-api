7cf2f7594b9d76cd7685d5791373efcfb09a6c75
Ansderly Rameau <rameauhansderly@gmail.com>
2023-09-05 00:12:22 -0400

    Add delete route for group member

--- src/routes/group-member.ts
+++ src/routes/group-member.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
-import { addMember, listMember } from '../controllers';
+import { addMember, deleteMemberFromGroup, listMember } from '../controllers';
 
 const router = express.Router();
 
 router.post('/api/groups/:group_id/members', addMember);
 router.get('/api/groups/:group_id/members', listMember);
-router.delete('/api/groups/:group_id/members/:member_id');
+router.delete('/api/groups/:group_id/members/:user_id', deleteMemberFromGroup);
 
 export default router;
