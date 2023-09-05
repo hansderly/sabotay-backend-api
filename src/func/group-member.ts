0472e9950629a360337902b90bc9ff408f95d939
Ansderly Rameau <rameauhansderly@gmail.com>
2023-09-05 00:09:43 -0400

    Add deleteMember function for groupMember controller

--- src/func/group-member.ts
+++ src/func/group-member.ts
@@ -2,9 +2,7 @@ import { prisma } from '../db';
 import { GroupMember } from '../interface';
 
 const getMember = async (groupMember: GroupMember) => {
-  const member = await prisma.groupMembers.findUnique({
-    where: { user_id: groupMember.user_id, group_id: groupMember.group_id },
-  });
+  const member = await prisma.groupMembers.findUnique({ where: groupMember });
 
   return member;
 };
@@ -17,4 +15,8 @@ const addMemberToGroup = async (groupMember: GroupMember) => {
 const listMemmberGroup = async (group_id: string) =>
   prisma.groupMembers.findMany({ where: { group_id }, select: { User: true } });
 
-export { getMember, addMemberToGroup, listMemmberGroup };
+const deleteMember = async (groupMember: GroupMember) => {
+  await prisma.groupMembers.delete({ where: groupMember });
+};
+
+export { getMember, addMemberToGroup, listMemmberGroup, deleteMember };
