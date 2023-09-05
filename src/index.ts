b7399824757066eef58778906b6a65ffb2780caa
Ansderly Rameau <rameauhansderly@gmail.com>
2023-09-04 23:29:55 -0400

    Add groupMemberRoutes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 
-import { authRoutes, groupRoutes, userRoutes } from './routes';
+import {
+  authRoutes,
+  groupRoutes,
+  userRoutes,
+  groupMemberRoutes,
+} from './routes';
 
 const app = express();
 app.use(express.json());
@@ -8,6 +13,7 @@ app.use(express.json());
 app.use(authRoutes);
 app.use(groupRoutes);
 app.use(userRoutes);
+app.use(groupMemberRoutes);
 
 const PORT = 2023;
 app.listen(PORT, () => console.log(`Server listening on PORT - ${PORT}`));
