cca549dbc13fe751f7435d69514b29a06a00ddf6
Ansderly Rameau <rameauhansderly@gmail.com>
2023-09-06 02:19:53 -0400

    Add api endpoint for contribution

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,5 +2,12 @@ import authRoutes from './auth';
 import groupRoutes from './group';
 import userRoutes from './user';
 import groupMemberRoutes from './group-member';
+import contributionRoutes from './contribution';
 
-export { authRoutes, groupRoutes, userRoutes, groupMemberRoutes };
+export {
+  authRoutes,
+  groupRoutes,
+  userRoutes,
+  groupMemberRoutes,
+  contributionRoutes,
+};
