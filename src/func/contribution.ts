1ff64fda2e746f6f508a1a34eccba4dbc4d753d2
Ansderly Rameau <rameauhansderly@gmail.com>
2023-09-06 02:18:38 -0400

    Add functions for contribution controller to use, such as: getContribution, addContribtution...

--- src/func/contribution.ts
+++ src/func/contribution.ts
@@ -1,1 +1,34 @@
 import { prisma } from '../db';
+import { GroupMember } from '../interface';
+
+const getContribution = async (id: string) =>
+  prisma.contribution.findUnique({ where: { id } });
+
+const addContribution = async ({ user_id, group_id }: GroupMember) => {
+  const contribution_date = '2023-08-29T12:00:00.000Z';
+  await prisma.contribution.create({
+    data: { user_id, group_id, contribution_date },
+  });
+};
+
+const listGroupContribution = async (group_id: string) => {
+  const contributions = await prisma.contribution.findMany({
+    where: { group_id },
+    select: {
+      user: true,
+    },
+  });
+
+  return contributions;
+};
+
+const deleteContribution = async (id: string, group_id: string) => {
+  await prisma.contribution.delete({ where: { id, group_id } });
+};
+
+export {
+  getContribution,
+  addContribution,
+  listGroupContribution,
+  deleteContribution,
+};
