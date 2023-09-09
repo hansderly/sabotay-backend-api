6afdd53455209ec0538c8a1c83c3d0fd5e0b5c66
Ansderly Rameau <rameauhansderly@gmail.com>
2023-09-09 01:30:28 -0400

    Change group member function to add value for key alias and user it for delete a group member

--- src/func/group-member.ts
+++ src/func/group-member.ts
@@ -1,22 +1,25 @@
 import { prisma } from '../db';
 import { GroupMember } from '../interface';
 
-const getMember = async (groupMember: GroupMember) => {
-  const member = await prisma.groupMember.findUnique({ where: groupMember });
+const getMember = async ({ user_id, group_id }: GroupMember) => {
+  const alias = `${user_id}-${group_id}`;
+  const member = await prisma.groupMember.findUnique({ where: { alias } });
 
   return member;
 };
 
-const addMemberToGroup = async (groupMember: GroupMember) => {
-  const data = { user_id: groupMember.user_id, group_id: groupMember.group_id };
+const addMemberToGroup = async ({ user_id, group_id }: GroupMember) => {
+  const alias = `${user_id}-${group_id}`;
+  const data = { alias, user_id, group_id };
   await prisma.groupMember.create({ data });
 };
 
 const listMemmberGroup = async (group_id: string) =>
   prisma.groupMember.findMany({ where: { group_id }, select: { user: true } });
 
-const deleteMember = async (groupMember: GroupMember) => {
-  await prisma.groupMember.delete({ where: groupMember });
+const deleteMember = async ({ user_id, group_id }: GroupMember) => {
+  const alias = `${user_id}-${group_id}`;
+  await prisma.groupMember.delete({ where: { alias } });
 };
 
 export { getMember, addMemberToGroup, listMemmberGroup, deleteMember };
