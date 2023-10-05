0e6e650704f448402a0754e265c694f3ba38f7a9
Ansderly Rameau <rameauhansderly@gmail.com>
2023-10-05 14:17:32 -0400

    Import errorHandler to index.ts and add it in app.use function

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ import {
   contributionRoutes,
 } from './routes';
 
+import { errorHandler } from './middlewares';
+
 const app = express();
 app.use(express.json());
 
@@ -17,6 +19,7 @@ app.use(groupRoutes);
 app.use(userRoutes);
 app.use(groupMemberRoutes);
 app.use(contributionRoutes);
+app.use(errorHandler);
 
 const PORT = 2023 || process.env.PORT;
 app.listen(PORT, () => console.log(`Server listening on PORT - ${PORT}`));
