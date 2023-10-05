8aff0664d440b1b3af4776f5ac7d1bcd50b8fb3d
Ansderly Rameau <rameauhansderly@gmail.com>
2023-10-05 14:19:00 -0400

    Catch error in auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -12,37 +12,47 @@ interface Login {
   password: string;
 }
 
-const register: RequestHandler = async (req, res) => {
+const register: RequestHandler = async (req, res, next) => {
   const input: Organizer = req.body;
-  const isOrganizerExist = await getOrganizer(input?.username);
+  try {
+    const isOrganizerExist = await getOrganizer(input?.username);
+    if (isOrganizerExist)
+      return res.status(409).json({ message: 'User already exist' });
 
-  if (isOrganizerExist)
-    return res.status(409).json({ message: 'User already exist' });
-
-  await createOrganizer(input);
-  return res.status(201).json({ message: 'User created successfully' });
+    await createOrganizer(input);
+    return res.status(201).json({ message: 'User created successfully' });
+  } catch (error: any) {
+    const err = new Error('Something went wrong while creating the organizer');
+    (err as any).status = 500;
+    next(err);
+  }
 };
 
-const login: RequestHandler = async (req, res) => {
+const login: RequestHandler = async (req, res, next) => {
   const input: Login = req.body;
-  const organizer = await getOrganizer(input?.username);
+  try {
+    const organizer = await getOrganizer(input?.username);
+    if (!organizer)
+      return res.status(401).json({ message: 'No user with that username' });
 
-  if (!organizer)
-    return res.status(401).json({ message: 'No user with that username' });
+    const isPasswordMatch = comparePassword(input.password, organizer.password);
 
-  const isPasswordMatch = comparePassword(input.password, organizer.password);
+    if (!isPasswordMatch)
+      return res.status(401).json({ message: 'Password incorrect' });
 
-  if (!isPasswordMatch)
-    return res.status(401).json({ message: 'Password incorrect' });
+    const payload = {
+      id: organizer.id,
+      username: organizer.username,
+      role: 'ADMIN',
+    };
+    const token = generateToken(payload);
 
-  const payload = {
-    id: organizer.id,
-    username: organizer.username,
-    role: 'ADMIN',
-  };
-  const token = generateToken(payload);
-
-  return res.status(200).json({ message: 'User login successfully', token });
+    return res.status(200).json({ message: 'User login successfully', token });
+  } catch (error) {
+    const err = new Error('Something went wrong while logging the organizer');
+    (err as any).status = 500;
+    next(err);
+  }
 };
 
 export { register, login };
